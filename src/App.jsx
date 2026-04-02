cbb28642b439a2cbeb6d3ad35620fe4f80df73d4
Devindi Prasadini <devindiprasadini607@gmail.com>
2026-04-02 22:56:38 +0530

    color cubes🎨

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,23 @@ import TrendingProducts from './components/trendingProducts'
 function App(){
   return(
     <>
-    <div>
-      <TrendingProducts/>
-    </div>
+      <div className='w-[700px] h-[700px] border-[6px] flex justify-center item'>
+        <div className='w-[600px] h-[600px] bg-yellow-500 flex flex-col items-center justify-center'>
+          <div className='w-[70px] h-[70px] bg-red-600'></div>
+          <div className='w-[70px] h-[70px] bg-blue-600'></div>
+          <div className='w-[70px] h-[70px] bg-green-400 fixed top-[10px] left-[25px]'></div>
+          <div className='w-[70px] h-[70px] bg-orange-500'></div>
+          <div className='w-[70px] h-[70px] bg-white'></div>
+          <div className='w-[70px] h-[70px] bg-pink-500'></div>
+        </div>
+
+
+
+
+
+
+
+      </div>
     </>
   )
 } 
