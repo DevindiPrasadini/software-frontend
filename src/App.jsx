208be641b145dd1dd4026483c501b706d90a0fe9
Devindi Prasadini <devindiprasadini607@gmail.com>
2026-04-09 15:11:31 +0530

    day 15✏️

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProductCard from './components/productCard'
 import TrendingProducts from './components/trendingProducts'
 import HomePage from "./pages/homePage"
 import AdminPage from './pages/adminPage'
+import TestPage from './pages/test'
 import { Route, Routes } from 'react-router-dom'
 
 
@@ -14,7 +15,8 @@ function App(){
       <div className='w-full h-screen  flex justify-center item relative bg-primary text-secondary'>
       <Routes>
         <Route path='/' element={<HomePage />}/>
-        <Route path='/admin' element={<AdminPage/>}/>
+        <Route path='/admin/*' element={<AdminPage/>}/>
+        <Route path='/test' element={<TestPage/>}/>
       </Routes>
       </div>
     </>
